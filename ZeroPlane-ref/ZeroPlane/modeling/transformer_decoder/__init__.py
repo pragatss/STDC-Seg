760c31c85b49dd34ae1e8340cd3d67d737932775
pragatss <pragat.software.services@gmail.com>
2026-03-28 09:54:45 -0400

    remoev zeroplane-ref

--- ZeroPlane-ref/ZeroPlane/modeling/transformer_decoder/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# from .maskformer_transformer_decoder import StandardTransformerDecoder
-from .masked_transformer_decoder import MultiScaleMaskedTransformerDecoder
