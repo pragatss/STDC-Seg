35dd0933057b1715faa5729e55c1cd09d026e392
pragatss <pragat.software.services@gmail.com>
2026-03-28 09:54:45 -0400

    remoev zeroplane-ref

--- ZeroPlane-ref/demo/run.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python demo/demo.py \
-    --config-file configs/ZeroPlaneNYUV2/dust3r_large_dpt_bs16_50ep.yaml \
-    --input ./demo/cvpr_demo.png \
-    --out ./demo/demo_out \
-    --resize_w 640 \
-    --resize_h 480 \
-    --opts \
-    MODEL.WEIGHTS ./checkpoints/dust3r_encoder_released.pth \
--- ZeroPlane-ref/scripts/eval_dust3r_parallel_domain.sh
+++
@@ -1,9 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-FEAT_DIM=256
-python train_net.py \
-    --eval-only \
-    --num-gpus 1 \
-    --config-file configs/ZeroPlaneParallelDomain/dust3r_large_dpt_bs16_50ep.yaml \
-    MODEL.WEIGHTS ./checkpoints/dust3r_encoder_released.pth \
-    OUTPUT_DIR ./visualizations/parallel_domain_test_vis \
-    TEST.NO_VIS "True"
